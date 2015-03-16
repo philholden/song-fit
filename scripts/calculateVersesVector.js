1b146fa0b464d116da165dad73815cdab1170e25
Phil Holden <philip.holden@ncr.com>
2015-03-16 23:01:16 +0000

    decouple

---
+++ scripts/calculateVersesVector.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var canvasGetBounds = require('./canvasGetBounds');
+var Transform = require('./Transform');
+
+//Takes a song that has been meta info about verse metrics and renders
+//each verse to a canvas. w and h is the desired width and height
+//the function has two passes the first pass renders larger than
+//desired size in order to get better metrics. The second pass is
+//at the desired size.
+
+function renderVersesVector(song, w, h, bounds) {
+  var aspect = w / h;
+  var canvas = document.createElement('canvas');
+  var mLineHeight = song.fontMetrics.h * song.lineHeight;
+  var sf = aspect > song.aspect ? h / song.pxHeight : w / song.maxWidth;
+  var ctx;
+  var padding = 4;
+  var trans = new Transform();
+  var transInv;
+  var trueBounds;
+
+  function drawVerse(verse, ctx) {
+    var lineNum = 0;
+    verse.lines.forEach(function(line) {
+      line.brokenLine.split('\n').forEach(function(fragment) {
+        ctx.fillText(fragment, 0, song.fontMetrics.ascent + lineNum * mLineHeight);
+        lineNum++;
+      });
+    });
+  }
+
+  canvas.setAttribute('id', 'canvas-super');
+
+  //bounds true on second pass
+  if (bounds) {
+    sf = aspect > bounds.w / bounds.h ? h / (bounds.h/(sf*1.1)) : w / (bounds.w/(sf*1.1));
+    canvas.width = Math.ceil(sf * song.maxWidth) + padding * 2;
+    canvas.height = Math.ceil(sf * song.pxHeight) + padding * 2;
+  } else {
+    sf *= 1.1;
+    canvas.width = Math.ceil(sf * song.maxWidth) + padding * 2;
+    canvas.height = Math.ceil(sf * song.pxHeight) + padding * 2;
+  }
+
+  ctx = canvas.getContext('2d');
+  ctx.font = song.fontHeight + 'px ' + song.fontName;
+  ctx.fillStyle = '#fff';
+  ctx.save();
+  trans.scale(sf,sf);
+  trans.translate(padding/sf, padding/sf);
+  transInv = trans.getInverseTransform();
+  ctx.setTransform.apply(ctx, trans.m);
+
+  //draw all verses on top of each other
+  song.verses.forEach(function(verse){drawVerse(verse,ctx);});
+
+  if (bounds) {
+    bounds = canvasGetBounds(canvas);
+    trueBounds = transInv.transformPoint(bounds.x, bounds.y);
+    trueBounds.w = bounds.w/sf;
+    trueBounds.h = bounds.h/sf;
+    trueBounds.sf = sf;
+    trueBounds.song = song;
+    trueBounds.mLineHeight = mLineHeight;
+    return trueBounds;
+  } else {
+    bounds = canvasGetBounds(canvas);
+    console.log(bounds);
+    return renderVersesVector(song, w, h, bounds);
+  }
+}
+
+module.exports = renderVersesVector;
---
+++ scripts/fillVerse.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var Transform = require('./Transform');
+var merge = require('merge');
+
+//metric output from renderVerseVector
+//
+
+var defaults = {
+  skewX: 0,
+  skewY: 0,
+  rotate: 0,
+  scaleX: 1,
+  scaleY: 1,
+  handleX:0.5,
+  handleY:0.5,
+  posX:0,
+  posY:0,
+  fill: '#ccc'
+};
+
+function fillVerse(ctx, metrics, conf) {
+
+  function drawVerse(verse) {
+    var lineNum = 0;
+    verse.lines.forEach(function(line) {
+      line.brokenLine.split('\n').forEach(function(fragment) {
+        ctx.fillText(fragment, 0, metrics.song.fontMetrics.ascent + lineNum * metrics.mLineHeight);
+        lineNum++;
+      });
+    });
+  }
+
+  ctx.save();
+  conf = conf || {};
+  conf = merge(true, conf);
+  conf = merge(conf, defaults);
+  conf.fill = '#ccc';
+
+  var verseTrans = new Transform();
+  var trans;
+  var inverse;
+  verseTrans.scale(metrics.sf, metrics.sf);
+  verseTrans.translate(-metrics.x, -metrics.y);
+  verseTrans.scale(conf.scaleX, conf.scaleY);
+  verseTrans.translate(
+    -(metrics.w * conf.handleX),
+    -(metrics.h * conf.handleY)
+  );
+
+  verseTrans.skewX(conf.skewX);
+  verseTrans.skewY(conf.skewY);
+
+  verseTrans.rotate(conf.rotate); //angle
+  inverse = verseTrans.getInverseTransform();
+  trans = inverse.transformPoint(conf.posX, conf.posY);
+  verseTrans.translate(trans.x, trans.y);
+  verseTrans.translate(-metrics.w * conf.handleX,-metrics.h * conf.handleY); //origin
+
+  ctx.font = metrics.song.fontHeight + 'px ' + metrics.song.fontName;
+  ctx.fillStyle = conf.fill();
+  //ctx.fillStyle = '#xxx'.replace(/x/g, x => (Math.random()*16|0).toString(16));
+  ctx.setTransform.apply(ctx, verseTrans.m);
+
+  drawVerse(metrics.song.verses[1], ctx);
+  ctx.restore();
+}
+
+
+
+module.exports = fillVerse;
